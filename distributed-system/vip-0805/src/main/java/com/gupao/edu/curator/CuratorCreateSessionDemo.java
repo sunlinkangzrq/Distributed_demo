b37577602967449146bb922b45505a0fb78db01c
sunlinkang <sunlinkang@LAPTOP-N6RMCEU4>
2019-12-26 17:48:32 +0800

    curator  操作1

---
+++ distributed-system/vip-0805/src/main/java/com/gupao/edu/curator/CuratorCreateSessionDemo.java
@@ -0,0 +1,23 @@
+package com.gupao.edu.curator;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.RetrySleeper;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+public class CuratorCreateSessionDemo {
+	
+	private static final  String  CONNECTING="106.54.165.85:2181";
+	
+	public static void main(String[] args) {
+		CuratorFramework  curatorFramework=CuratorFrameworkFactory
+				.newClient(CONNECTING, 5000, 5000, new ExponentialBackoffRetry(1000, 3));
+		curatorFramework.start();
+		CuratorFramework  curatorFramework1=CuratorFrameworkFactory.builder().connectString(CONNECTING)
+											.sessionTimeoutMs(5000).connectionTimeoutMs(5000)
+											.retryPolicy(new ExponentialBackoffRetry(1000,3)).build();
+		curatorFramework1.start();
+		System.out.println("success");
+	}
+}
