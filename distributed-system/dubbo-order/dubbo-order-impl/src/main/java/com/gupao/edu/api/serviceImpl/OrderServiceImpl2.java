6ed866f883beb58e7e8a2f9e21ae00fd780b3b69
sunlinkang <sunlinkang@LAPTOP-N6RMCEU4>
2020-01-19 17:33:59 +0800

    dubbo  学习2

--- distributed-system/dubbo-order/dubbo-order-impl/src/main/java/com/gupao/edu/api/serviceImpl/OrderServiceImpl2.java
+++ distributed-system/dubbo-order/dubbo-order-impl/src/main/java/com/gupao/edu/api/serviceImpl/OrderServiceImpl2.java
@@ -1,5 +1,7 @@
 package com.gupao.edu.api.serviceImpl;
 
+import java.util.concurrent.TimeUnit;
+
 import com.gupao.edu.dto.OrderRequest;
 import com.gupao.edu.dto.OrderResponse;
 import com.gupao.edu.service.IOrderService;
@@ -8,6 +10,12 @@ public class OrderServiceImpl2 implements IOrderService{
 
 	public OrderResponse doOrder(OrderRequest request) {
 		// TODO Auto-generated method stub
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("曾将来过 版本2"+request.toString());
 		OrderResponse res=new OrderResponse();
 		res.setCode(200);
